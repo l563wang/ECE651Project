066ae91faaaaa2b6ebfaf89e8615ab641cb9d1f9
tonzmy <ultracell@outlook.com>
2020-02-27 17:01:24 -0500

    check gitignore

--- app/src/main/java/com/libangliang/ece651project/LoginActivity.java
+++ app/src/main/java/com/libangliang/ece651project/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.libangliang.ece651project;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
